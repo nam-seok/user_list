730951c7da584600f560f9f9143f912f3f29d37a
nsbaee <znxpsxk8@navere.com>
2020-06-17 17:39:45 +0900

    first commit

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 var express = require('express')
 var path = require('path')
-var mysql = require('mysql')
-const { config } = require('process')
+
 var app = express()
-
-sql_config = {
-    host:'localhost',
-    user:'root',
-    password:'1234',
-    database:'o2'
-}
-var db = mysql.createConnection(sql_config)
-
-db.connect()
+var apiRouter = require('./routes/api_router')
+//db.connect()
 
 app.set('views', path.resolve(__dirname + '/views'))
 app.set('view engine', 'ejs')
@@ -20,26 +11,28 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
-app.get('/hello', (request, response)=>{
-    var name = "배남석"
-    //console.log(request)
-    response.render("hello.ejs",{data:name})
-    //response.send("hello world")
-})
+app.use('/', apiRouter)
 
-app.get('/data', (req, res)=>{
-    var sql = 'SELECT * FROM topic'
-    db.query(sql , (err , result)=>{
-        if(err){
-        console.log(err)
-    } else {
-        console.log(result[0].description)
+//app.get('/hello', (request, response)=>{
+  //  var name = "배남석"
+    ////console.log(request)
+   // response.render("hello.ejs",{data:name})
+    //response.send("hello world")
+// })
+
+//app.get('/data', (req, res)=>{
+ //   var sql = 'SELECT * FROM topic'
+ //   db.query(sql , (err , result)=>{
+ //       if(err){
+  //      console.log(err)
+  //  } else {
+ //       console.log(result[0].description)
         //res.send(result[0].author+'수업은'+result[0].title)
         //res.send(`${result[0].author} 수업은 ${result[0].title}`)
-        res.render('data', {result:result})
-    }
-})
-})
+ //       res.render('data', {result:result})
+ //   }
+//)
+//})
 
     //console.log(path.join(__dirname + '/views'))
 
