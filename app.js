4bdc2fc1d49ac934129eb84d5418dc77ae156bc3
nsbaee <znxpsxk8@navere.com>
2020-06-17 12:18:49 +0900

    seconf

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 var express = require('express')
-
+var path = require('path')
 var app = express()
+
+app.set('views', path.resolve(__dirname + '/views'))
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded())
+app.use(express.json())
+
+app.get('/hello', (request, response)=>{
+    console.log(request)
+    response.send("hello world")
+})
+
+    //console.log(path.join(__dirname + '/views'))
 
 var port = 8000;
 
-app.listen(port, function() {
+app.listen(port, ()=>{
     console.log('server is Runing at http://localhost:8080')
 
 })
