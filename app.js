c68ae747e196dbbc902bafab5e84af2381ffd125
nsbaee <znxpsxk8@navere.com>
2020-06-17 15:33:07 +0900

    first commit

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
 var express = require('express')
 var path = require('path')
+var mysql = require('mysql')
+const { config } = require('process')
 var app = express()
+
+sql_config = {
+    host:'localhost',
+    user:'root',
+    password:'1234',
+    database:'o2'
+}
+var db = mysql.createConnection(sql_config)
+
+db.connect()
 
 app.set('views', path.resolve(__dirname + '/views'))
 app.set('view engine', 'ejs')
 
-app.use(express.urlencoded())
+app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 app.get('/hello', (request, response)=>{
-    console.log(request)
-    response.send("hello world")
+    var name = "배남석"
+    //console.log(request)
+    response.render("hello.ejs",{data:name})
+    //response.send("hello world")
+})
+
+app.get('/data', (req, res)=>{
+    var sql = 'SELECT * FROM topic'
+    db.query(sql , (err , result)=>{
+        if(err){
+        console.log(err)
+    } else {
+        console.log(result[0].description)
+        //res.send(result[0].author+'수업은'+result[0].title)
+        //res.send(`${result[0].author} 수업은 ${result[0].title}`)
+        res.render('data', {result:result})
+    }
+})
 })
 
     //console.log(path.join(__dirname + '/views'))
@@ -18,6 +46,6 @@ app.get('/hello', (request, response)=>{
 var port = 8000;
 
 app.listen(port, ()=>{
-    console.log('server is Runing at http://localhost:8080')
+    console.log('server is Runing at http://localhost:8000')
 
 })
